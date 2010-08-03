4892baa5b37d9e880c9fd9b58bcf7ac3373b758d
Mingli Yuan <mingli.yuan@gmail.com>
2010-08-03 18:30:13 +0800

    fix json

--- app/redirect.js
+++ app/redirect.js
@@ -17,38 +17,39 @@ exports.app = function(env) {
     request.on('response', function (response) {
       response.setEncoding('utf-8');
       response.on('data', function (chunk) {
-        var sandbox = {};
-        var code = 'val = ' + chunk + ';'
-        Script = process.binding('evals').Script;
-        Script.runInNewContext(code, sandbox, '');
-        if(sandbox.val) {
-            var data = sandbox.val;
-            var url = undefined;
-            for(var pageId in data.query.pages) {
-              if(pageId > 0) {
-                var page = data.query.pages[pageId];
-                url = page.fullurl;
-                break;
-              }
+        var url = undefined;
+        try {
+          chunk = chunk.toString('utf8');
+          chunk = chunk.substring(1, chunk.length - 1);
+          require('sys').puts(chunk);
+          var data = JSON.parse(chunk);
+          for(var pageId in data.query.pages) {
+            if(pageId > 0) {
+              var page = data.query.pages[pageId];
+              url = page.fullurl;
+              break;
             }
-            if(url) {
-              res.redirect(url);
-            } else {
-              var html = '';
-              html += '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-              html += '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
-              html +=     '<head>';
-              html +=        '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
-              html +=         '<title>Unsupported Language</title>';
-              html +=         '<link rel="stylesheet" href="/styles/style.css" type="text/css" media="screen" charset="utf-8" />';
-              html +=     '</head>';
-              html +=     '<body>';
-              html +=         '<h2 id="title">Sorry, network problems!</h2>';
-              html +=         '<p id="copy">by Mingli Yuan</p>';
-              html +=     '</body>';
-              html += '</html>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-              res.simpleHtml(200, html);
-            }
+          }
+        } catch (e) {
+          require('sys').puts("error when parsing json: " + e);
+        }
+        if(url) {
+          res.redirect(url);
+        } else {
+          var html = '';
+          html += '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+          html += '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
+          html +=     '<head>';
+          html +=        '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+          html +=         '<title>Network Problems</title>';
+          html +=         '<link rel="stylesheet" href="/styles/style.css" type="text/css" media="screen" charset="utf-8" />';
+          html +=     '</head>';
+          html +=     '<body>';
+          html +=         '<h2 id="title">Sorry, network problems!</h2>';
+          html +=         '<p id="copy">by Mingli Yuan</p>';
+          html +=     '</body>';
+          html += '</html>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+          res.simpleHtml(200, html);
         }
       });
     });
