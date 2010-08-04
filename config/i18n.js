04dc64e05282da42a9c234e25f60e6effc917069
Mingli Yuan <mingli.yuan@gmail.com>
2010-08-04 18:50:29 +0800

    add ml language

--- config/i18n.js
+++ config/i18n.js
@@ -1,25 +1,32 @@
 exports.i18n = {
-  supported: ['cs', 'en', 'zh'],
+  supported: ['cs', 'en', 'ml', 'zh'],
   msg: {
     cs: {
       title: 'Zkracovač adres Wikipedie',
       lable: 'Název: ',
       wiki: 'Wiki',
-      shortit: 'Zkrátit!',
+      shorten: 'Zkrátit!',
       notfound: 'Článek nebyl nalezen!'
     },
     en: {
       title: 'Wikipedia Shorturl Service',
       lable: 'Title: ',
       wiki: 'Wiki',
-      shortit: 'Short It!',
+      shorten: 'Shorten It!',
       notfound: 'Article was not found!'
+    },
+    ml: {
+      title: 'വിക്കിപീഡിയ ചെറിയ യു.ആർ.എൽ. സേവനം',
+      lable: 'തലക്കെട്ട്: ',
+      wiki: 'വിക്കി',
+      shorten: 'ചെറുതാക്കൂ!',
+      notfound: 'ലേഖനം കണ്ടെത്താനായില്ല!'
     },
     zh: {
       title: '维基缩址服务',
       lable: '标题： ',
       wiki: '维基',
-      shortit: '缩址！',
+      shorten: '缩址！',
       notfound: '未找到条目！'
     }
   }
