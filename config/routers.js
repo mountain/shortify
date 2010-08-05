152e963d8d4c96126293d681800a6f00ede02ce1
Mingli Yuan <mingli.yuan@gmail.com>
2010-08-05 22:39:09 +0800

    secure url pattern

--- config/routers.js
+++ config/routers.js
@@ -1,5 +1,5 @@
 exports.routers = {
   main: new RegExp("^/$"),
-  shorturl: new RegExp("^/s/(.*)$"),
-  redirect: new RegExp("^/r/([^/]+)/([^/]+)$")
+  shorturl: new RegExp("^/s/([a-z]*)$"),
+  redirect: new RegExp("^/r/([a-z]+)/([0-9a-z]+)$")
 };
