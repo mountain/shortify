36c02bf469212ed80d34fcd7743e571c531a2745
Mingli Yuan <mingli.yuan@gmail.com>
2010-08-05 20:35:23 +0800

    fix encode issue

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,8 +2,8 @@ function app() {
   $("#submit").click(function() {
     var title = $("#query").val();
     if(title && title !== "") {
-      title = escape(title);
-      var query = "http://" + lang + ".wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&callback=?&titles=" + title;
+      var encoded = escape(utf8(title));
+      var query = "http://" + lang + ".wikipedia.org/w/api.php?action=query&prop=info&inprop=url&format=json&callback=?&titles=" + encoded;
       $.getJSON(query, function(data) {
         if(!data.query) return;
         for(var pageId in data.query.pages) {
@@ -21,4 +21,26 @@ function app() {
     };
   });
 }
+
+function utf8(string) {
+  string = string.replace(/\r\n/g,"\n");
+  var utftext = "";
+  for (var n = 0; n < string.length; n++) {
+    var c = string.charCodeAt(n);
+    if (c < 128) {
+      utftext += String.fromCharCode(c);
+    }
+    else if((c > 127) && (c < 2048)) {
+      utftext += String.fromCharCode((c >> 6) | 192);
+      utftext += String.fromCharCode((c & 63) | 128);
+    }
+    else {
+      utftext += String.fromCharCode((c >> 12) | 224);
+      utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+      utftext += String.fromCharCode((c & 63) | 128);
+    }
+  }
+  return utftext;
+}
+
 
